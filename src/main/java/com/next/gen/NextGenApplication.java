22540c6534bbc66030466970cdb6fea8f857f64d
StevenNdaye <stevenndaye@gmail.com>
2016-06-01 16:19:26 +0200

    Steven: Bootstrapped the project for the next gen ecomm

---
+++ src/main/java/com/next/gen/NextGenApplication.java
@@ -0,0 +1,12 @@
+package com.next.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextGenApplication.class, args);
+	}
+}
